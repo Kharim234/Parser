0b18a61c27d83140819192f523157c8bdf346bf3
Malinowski <malinowskmichal27@gmail.com>
2020-04-26 15:08:11 +0200

    Add save file, and now parser get string to parse from list

--- Jabil_parser/mainwindow.cpp
+++ Jabil_parser/mainwindow.cpp
@@ -3,6 +3,7 @@
 #include <QFileDialog>
 #include <QFile>
 #include "parser_files.h"
+#include <QTextStream>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -16,15 +17,13 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::parse_for_limits_and_show_them(QString String_with_limits){
+void MainWindow::parse_for_limits_and_show_them(QString String_with_limits, QStringList Limits_to_parse){
     Parser_files internal_parser;
     QStringList Limits_to_show;
-    internal_parser.Parse_string("upper", String_with_limits);
-    Limits_to_show.append(internal_parser.Get_parsed_string());
-    internal_parser.Parse_string("nominal", String_with_limits);
-    Limits_to_show.append(internal_parser.Get_parsed_string());
-    internal_parser.Parse_string("lower", String_with_limits);
-    Limits_to_show.append(internal_parser.Get_parsed_string());
+    for(int i  = 0; i < Limits_to_parse.size(); i++){
+        internal_parser.Parse_string(Limits_to_parse.at(i), String_with_limits);
+        Limits_to_show.append(internal_parser.Get_parsed_string());
+    }
     show_string_list_in_one_line(Limits_to_show);
 }
 
@@ -44,32 +43,71 @@ void MainWindow::on_pushButton_Parse_File_clicked()
 {
     QString fileName = QFileDialog::getOpenFileName(this,
         tr("Select log files"), "/home/jana", tr("Log files (*.xdcf *.*)"));
+    ui->textBrowser->clear();
+    m_Parse_file_path.clear();
 
     QFile file(fileName);
-    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)){
+        ui->textBrowser->append("File read error");
         return;
+    }
+    m_Parse_file_path += fileName;
 
     QByteArray Text_in_file = file.readAll();
     file.close();
+
     Parser_files external_parser, internal_parser;
     int file_index;
     QString Parsed_string;
-    QStringList Limits_to_parse = {"upper", "nominal", "lower"};
+    QStringList Limits_to_parse = {"subDesignator","upper", "nominal", "lower"};
+    QString external_XML_mark_string_to_parse = "limits";
 
-    if(external_parser.Parse_string("limits",Text_in_file) > 0){
+    if(external_parser.Parse_string(external_XML_mark_string_to_parse,Text_in_file) > 0){
         show_string_list_in_one_line(Limits_to_parse);
+    }else{
+        ui->textBrowser->append("Not found any limits");
+        return;
     }
 
     do{
-        file_index = external_parser.Parse_string("limits",Text_in_file);
+        file_index = external_parser.Parse_string(external_XML_mark_string_to_parse,Text_in_file);
         Parsed_string = external_parser.Get_parsed_string();
         Text_in_file.remove(0, file_index);
 
         if(file_index >= 0){
           Parsed_string = external_parser.Get_parsed_string();
-          this->parse_for_limits_and_show_them(Parsed_string);
+          this->parse_for_limits_and_show_them(Parsed_string, Limits_to_parse);
         }
 
     }while(file_index>=0);
 
 }
+
+void MainWindow::on_pushButton_Save_File_clicked()
+{
+
+//    QString fileName = QFileDialog::getOpenFileName(this,
+//        tr("Save file"), "/home/jana", tr("save as (*.csv)"));
+    QString Parse_file_path_with_changed_ending, Parse_file_path = m_Parse_file_path;
+    if(Parse_file_path.length()>0){
+        Parse_file_path_with_changed_ending = (Parse_file_path.split(".")).at(0);
+        Parse_file_path_with_changed_ending +=  ".csv";
+    }else{
+       Parse_file_path_with_changed_ending = "/home/untitled.csv";
+    }
+
+    QString fileName = QFileDialog::getSaveFileName(this, tr("Save File"),
+                               Parse_file_path_with_changed_ending,
+                               tr("CSV file (*.csv)"));
+
+    QFile file(fileName);
+    if (!file.open(QIODevice::WriteOnly | QIODevice::Text)){
+        //ui->textBrowser->clear();
+        //ui->textBrowser->append("Error - save file");
+        return;
+    }
+    QTextStream out(&file);
+    out << ui->textBrowser->toPlainText();
+
+}
+
--- Jabil_parser/mainwindow.h
+++ Jabil_parser/mainwindow.h
@@ -7,6 +7,7 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -19,9 +20,13 @@ private slots:
     void on_pushButton_Parse_File_clicked();
 
 
+    void on_pushButton_Save_File_clicked();
+
 private:
     Ui::MainWindow *ui;
-    void parse_for_limits_and_show_them(QString String_with_limits);
+    void parse_for_limits_and_show_them(QString String_with_limits, QStringList Limits_to_parse);
     void show_string_list_in_one_line(QStringList List);
+    QString m_Parse_file_path;
 };
 #endif // MAINWINDOW_H
+
